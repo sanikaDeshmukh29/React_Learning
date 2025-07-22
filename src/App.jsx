1cb1bafc0327672b7046f92e55bc9b8c0f9802a3
sanikaDeshmukh29 <sanikadesh29@gmail.com>
2025-07-22 21:50:20 +0530

    Understanding returning array for multiple elements without creating extra parent element

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
  export const App = () => {
  
    return (
-     <div>
-       
-       <NetflixSries />
-       <NetflixSries />
-       <NetflixSries />
-       <NetflixSries />
-       <NetflixSries />
-     
-     </div>
+    [<NetflixSries key = "1" />,<NetflixSries key = "2" />,<NetflixSries key = "3" />,<NetflixSries key = "4" />]
    )
 };
 
+// there are two ways to remove the parent elemnet which create uneccessary dom node
+// 1 . Array return
+// 2. fragments 
 
 const NetflixSries = () => {
 
