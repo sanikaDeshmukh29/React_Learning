ad676b19b5cac0fd7e07cb5655131c85173c989e
sanikaDeshmukh29 <sanikadesh29@gmail.com>
2025-07-22 15:03:18 +0530

    First card creation using react
    card creation only to understand the structure without css

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+ export const App = () => {
+ 
+   return( <div>
+     <div>
+       <img src="Queen of tears.jpeg" alt="queen_of_tears.jpeg" width = "40%" height = "40%"git status />
+     </div>
+     <h1>Name: Queen of Tears</h1>
+     <h1>Rating: 8.2</h1>
+     <p>Summary: Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque dolore fugit repellendus quis praesentium veritatis eligendi nulla deserunt.
+       Quibusdam maxime veritatis architecto ad officiis earum, aliquam enim possimus aut?</p>
+  </div>
+   )
+};
+
+// notes
+
+// Each jsx element must have one parent element, and retur only one parent element
+
+// every tag in jsx needs to be closed eg . img - you  need to slef close it />
