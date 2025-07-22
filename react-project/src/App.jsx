850518be97e90d125262b0fd5bc7df7f1640bd50
sanikaDeshmukh29 <sanikadesh29@gmail.com>
2025-07-22 12:29:05 +0530

    Updated App.jsx

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -3,3 +3,10 @@
   return <h1>Hello , Sanika Deshmukh!</h1>
 
 };
+
+// console.log(React.createElement('h1', null, "Hello , Sanika Deshmukh")); // behind the scene...
+
+// jsx (we can write html in javascript)
+
+// so the jsx helps us to write the html in javascript/react,, & to  place the  elements in dom without any 
+// createElement() and appendChild() methods
