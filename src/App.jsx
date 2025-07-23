395449cab44913e0ed6da7addbe945dc44622ef2
sanikaDeshmukh29 <sanikadesh29@gmail.com>
2025-07-23 16:45:03 +0530

    Dynamic values understanding using - variables

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,27 @@ export const App = () => {
    )
 };
 
-// there are two ways to remove the parent elemnet which create uneccessary dom node
-// 1 . Array return
-// 2. fragments 
+// Implementing  dynamic values
+
+//? you  can embed any javascript variable within jsx by enclosing 
+// it  in a curly braces{}. The value of the  varibale will be inserted into the DOM 
+// at the respective location
 
 const NetflixSries = () => {
+
+  const name = "Queen of Tears";
+  const rating = "8.2";
+  const summary =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque dolorb hbdabduwi nkdhciwuhvibvwwb";
 
   return (
     <div>
      <div>
        <img src="queen_of_tears.jpeg" alt="queen_of_tears.jpeg" width = "40%" height = "40%"git status />
      </div>
-     <h1>Name: Queen of Tears</h1>
-     <h1>Rating: 8.2</h1>
-     <p>Summary: Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque dolore fugit repellendus quis praesentium veritatis eligendi nulla deserunt.
-       Quibusdam maxime veritatis architecto ad officiis earum, aliquam enim possimus aut?</p>
+      <h1>Name: {name}</h1>
+     <h1>Rating:{rating} </h1>
+      <p>Summary:{summary}</p>
   </div>
   )
 
